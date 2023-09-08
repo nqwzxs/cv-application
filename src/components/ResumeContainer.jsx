4032037cd428cdaba583f514a34c1c5d415b4a1f
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-09-08 19:39:31 +0500

    add resume container styling

--- src/components/ResumeContainer.jsx
+++ src/components/ResumeContainer.jsx
@@ -1,7 +1,7 @@
 import '../styles/ResumeContainer.css'
 
 function ResumeContainer() {
-  return <div className="resume-container"></div>
+  return <div className='resume-container'></div>
 }
 
 export default ResumeContainer
