bc0d7b914a44024f6183d250ee28c4077c791246
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-09-08 09:47:11 +0500

    add forms component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
+import Forms from './components/Forms'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Forms />
+  </React.StrictMode>,
 )
