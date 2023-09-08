a891d89081bb2cc252b4da3f7b51933c46754a8f
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-09-08 19:36:16 +0500

    add input group styling

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,7 +1,9 @@
+import '../styles/InputGroup.css'
+
 function InputGroup({ labelText, type, id }) {
   return (
-    <div className="input-group">
-      <label htmlFor="school-name">{labelText}</label>
+    <div className='input-group'>
+      <label htmlFor='school-name'>{labelText}</label>
       {type === 'textarea' ? (
         <textarea id={id}></textarea>
       ) : (
